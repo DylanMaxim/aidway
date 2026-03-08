5234ca8a660a3ef10ebd92d727ca8df77b9b0328
jamesschaffer <james.t.schaffer@gmail.com>
2026-03-08 11:56:58 +0000

    rebuid navbar

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,9 +1,13 @@
-import { Navbar } from '@/components/user/navbar'
+import { Navbar } from '@/components/default/navbar'
+import { SuperButton } from '@/components/default/button'
 
 export default function Home() {
 	return (
 <>
-	<Navbar/>
+	<Navbar><>
+		<SuperButton name="Request Supplies" path="/submitRequest" variant={1}></SuperButton>
+		<SuperButton name="Manage group" path="/group" variant={1}></SuperButton>
+	</></Navbar>
 	<h3>A period doesn't stop with a crisis.</h3>
 	<p>AIDWAY aims to deliver period products during humanitarian disasters. Around the world, women, girls, and people who menstruate require sanitary products more than other types of products to manage their periods safely and with dignity.</p>
 </>
