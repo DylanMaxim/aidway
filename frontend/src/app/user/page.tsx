436800728d7ef521e6958727f317a99e8e00a67a
sp946 <sp946@student.le.ac.uk>
2026-03-08 11:18:10 +0000

    added steps on user dashboard

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -4,7 +4,16 @@ export default function Home() {
 	return (
 <>
 	<Navbar/>
-	<h3>A period doesn't stop with a crisis.</h3>
-	<p>AIDWAY aims to deliver period products during humanitarian disasters. Around the world, women, girls, and people who menstruate require sanitary products more than other types of products to manage their periods safely and with dignity.</p>
+	<h2 className="text-black py-5 rounded-lg font-semibold">Periods don't stop in a crisis.</h2>
+	<p>AIDWAY aims to deliver period products during humanitarian disasters. Find out how to help.</p>
+
+	<p>To register with us...</p>
+	<ol>
+		<li>Users in a camp must register with a charity.</li>
+		<li>A charity should register with us, and they will receive an access code.</li>
+		<li>Each camp will have a unique access code.</li>
+		<li>All users in different camps will be able to submit a request form to request what products will be need.</li>
+	</ol>
+
 </>
 );}
