4ae0c60818a9372ce4a805a479362d217968e3ee
sp946 <sp946@student.le.ac.uk>
2026-03-08 12:07:19 +0000

    edited font on user dashboard

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -8,12 +8,19 @@ export default function Home() {
 	<h2 className="text-red py-5 rounded-lg font-sans font-semibold text-xl">Periods don't stop in a crisis.</h2>
 	<p>AIDWAY aims to deliver period products during humanitarian disasters. Find out how to help.</p>
 
-	<p className="text-red text-base font-sans">To register with us...</p>
-	<ol className="text-red text-base font-sans">
+	<p className="text-red py-3 rounded-lg text-base font-sans">To register with us...</p>
+	<ol className="text-red py-3 rounded-lg text-base font-sans">
 		<li>1. Users in a camp must register with a charity.</li>
 		<li>2. A charity should register with us, and they will receive an access code.</li>
 		<li>3. Each camp will have a unique access code.</li>
 		<li>4. All users in different camps will be able to submit a request form to request what products will be need.</li>
+	</ol>
+
+	<p className="text-red py-3 rounded-lg text-base"> After registration...</p>
+	<ol className="text-red py-3 rounded-lg text-base font-sans">
+		<li>1. After each camp receives an access code, each camp will be assigned a camp correspondent.</li>
+		<li>2. The camp correspondent will ensure that each user on the camp receives the products they requested.</li>
+		<li>When products are ordered, they will be delivered on a monthly basis.</li>
 	</ol>
 
 </>
